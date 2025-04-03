b13ba6a84dfda5aa8d1a251811fa4f6fa1755ce2
Polina Lukashenko <polinalukashenko12@gmail.com>
2025-04-03 22:29:27 +0300

    Update parse_linked_list_tostr.py
    in the previous commit I pasted a code from another task, here is the correct one

--- parse_linked_lst/parse_linked_list_tostr.py
+++ parse_linked_lst/parse_linked_list_tostr.py
@@ -1,28 +1,11 @@
 '''linked_list_from_string'''
 class Node:
-    def __init__(self, data, next=None):
+    def __init__(self, data, next=None): 
         self.data = data
         self.next = next
 
-def stringify(node):
-  """
-  a function that takes a linked list 
-  and returns a string out of it
-  """
-    outp = []
-    temp = node
-    while temp:
-        outp.append(str(temp.data))
-        temp = temp.next
-    if temp is None:
-        outp.append('None')
-    return ' -> '.join(outp)
-
 
 def linked_list_from_string(s):
-  """a function that takes a linked list which was stringified 
-  and returns a linked list again
-  """
     if s == 'None':
         return None
     spl = s.split(' -> ')[:-1]
